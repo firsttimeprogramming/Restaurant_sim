7a450a3b4615d9c163bc506785e9f18ae8b4e9cc
zheng24 <zheng24@CCL-GMLW110-37.ad.uwm.edu>
2018-04-10 23:37:37 -0500

    testing3

--- restaurant_sim/src/mainframe.java
+++ restaurant_sim/src/mainframe.java
@@ -1,10 +1,15 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 import javax.swing.*;
+import java.io.IOException;
 public class mainframe extends JFrame {
 	mainframe(){
 		this.setTitle("restuarant sim");
 		this.setBounds(0, 0, 600, 600);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	public static void main() throws IOException {
+		
 	}
 }
